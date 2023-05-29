d6bf0af67212e5f93bfd00c9a87d0bd1bc163f2a
DavidNIngebrethsen <111867398+DavidNIngebrethsen@users.noreply.github.com>
2023-05-29 19:17:54 +0200

    Another upload
    Making the mobile menu work

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function renderHead() {
     headNavPosts.href = "list.html"
     const contHeadNavPosts = document.createElement("div")
     contHeadNavPosts.classList.add("head-nav")
-    const textHeadNavPosts = document.createElement("p")
+    const textHeadNavPosts = document.createElement("h3")
     textHeadNavPosts.classList.add("head-nav-text")
     textHeadNavPosts.innerText = "Posts"
 
@@ -39,7 +39,7 @@ function renderHead() {
     headNavAbout.href = "about.html"
     const contHeadNavAbout = document.createElement("div")
     contHeadNavAbout.classList.add("head-nav")
-    const textHeadNavAbout = document.createElement("p")
+    const textHeadNavAbout = document.createElement("h3")
     textHeadNavAbout.classList.add("head-nav-text")
     textHeadNavAbout.innerText = "About"
 
@@ -50,7 +50,7 @@ function renderHead() {
     headNavContact.href = "contact.html"
     const contHeadNavContact = document.createElement("div")
     contHeadNavContact.classList.add("head-nav")
-    const textHeadNavContact = document.createElement("p")
+    const textHeadNavContact = document.createElement("h3")
     textHeadNavContact.classList.add("head-nav-text")
     textHeadNavContact.innerText = "Contact"
 
@@ -81,6 +81,7 @@ function renderMobileHead () {
 
     const headMenuButton = document.createElement("button")
     headMenuButton.classList.add("head-menu-button")
+    headMenuButton.addEventListener("click", () => menuOpen())
     const iconHeadMenuButton = document.createElement("i")
     iconHeadMenuButton.classList.add("fa-solid", "fa-bars")
 
@@ -90,24 +91,43 @@ function renderMobileHead () {
     menuBody.classList.add("menu-body")
     const menuMenuButton = document.createElement("button")
     menuMenuButton.classList.add("menu-menu-button")
+    menuMenuButton.addEventListener("click", () => menuClose())
     const iconMenuMenuButton = document.createElement("i")
     iconMenuMenuButton.classList.add("fa-solid", "fa-bars")
     menuMenuButton.append(iconMenuMenuButton)
+
     const menuNavPosts = document.createElement("a")
     menuNavPosts.href = "list.html"
-    menuNavPosts.innerText = "Posts"
+    const textMenuNavPosts = document.createElement("h3")
+    textMenuNavPosts.innerText = "Posts"
+    textMenuNavPosts.classList.add("text-menu-nav")
+    menuNavPosts.append(textMenuNavPosts)
     const menuNavAbout = document.createElement("a")
     menuNavAbout.href = "about.html"
-    menuNavAbout.innerText = "About"
+    const textMenuNavAbout = document.createElement("h3")
+    textMenuNavAbout.innerText = "About"
+    textMenuNavAbout.classList.add("text-menu-nav")
+    menuNavAbout.append(textMenuNavAbout)
     const menuNavContact = document.createElement("a")
     menuNavContact.href = "contact.html"
-    menuNavContact.innerText = "Contact"
+    const textMenuNavContact = document.createElement("h3")
+    textMenuNavContact.innerText = "Contact"
+    textMenuNavContact.classList.add("text-menu-nav")
+    menuNavContact.append(textMenuNavContact)
+
+    
     
     menuBody.append(menuMenuButton, menuNavPosts, menuNavAbout, menuNavContact)
-
     headBody.append(homeLink, headMenuButton, menuBody)
 
     document.querySelector("header").append(headBody)
+}
+
+function menuOpen() {
+    document.querySelector(".menu-body").style.display = "flex"
+}
+function menuClose() {
+    document.querySelector(".menu-body").style.display = "none"
 }
 
 let menuActive = false;
